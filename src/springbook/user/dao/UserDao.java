ef7909b4c83af6ffc236de3db227f459720cdb92
LachesisJin <jjinimania@gmail.com>
2013-11-13 13:37:01 +0900

    change

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -1,58 +1,19 @@
 package springbook.user.dao;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import springbook.user.domain.User;
-
-import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: 2SsooN
- * Date: 2013. 11. 8.
- * Time: AM 12:13
- * To change this learningtest.template use File | Settings | File Templates.
- */
-public class UserDao {
-    private RowMapper<User> userMapper = new RowMapper<User>() {
-        @Override
-        public User mapRow(ResultSet rs, int i) throws SQLException {
-            User user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
-            return user;
-        }
-    };
+import springbook.user.domain.User;
 
-    private JdbcTemplate jdbcTemplate;
+public interface UserDao {
 
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+    void add(User user);
 
-    public void add(final User user) throws SQLException {
-        this.jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES(?, ?, ?)",
-                user.getId(), user.getName(), user.getPassword());
-    }
+    User get(String id);
 
-    public User get(String id) throws SQLException {
-        return this.jdbcTemplate.queryForObject("SELECT * FROM users WHERE id = ?",
-            new Object[] {id}, this.userMapper);
-    }
+    List<User> getAll();
 
-    public void deleteAll() throws SQLException {
-        this.jdbcTemplate.update("DELETE FROM users");
-    }
+    void deleteAll();
 
-    public int getCount() throws SQLException {
-        return this.jdbcTemplate.queryForInt("SELECT COUNT(*) FROM users");
-    }
+    int getCount();
 
-    public List<User> getAll() {
-        return this.jdbcTemplate.query("SELECT * FROM users ORDER BY id", this.userMapper);
-    }
 }
--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -1,37 +1,34 @@
 package springbook.user.dao;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import springbook.user.domain.User;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
 import java.util.List;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import javax.sql.DataSource;
 
-/**
- * Created with IntelliJ IDEA.
- * User: 2SsooN
- * Date: 2013. 11. 8.
- * Time: AM 1:39
- * To change this learningtest.template use File | Settings | File Templates.
- */
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import springbook.user.domain.User;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="/test-applicationContext.xml")
-@DirtiesContext
+@ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
     @Autowired
-    ApplicationContext context;
-
     private UserDao dao;
+    @Autowired
+    private DataSource dataSource;
 
     private User user1;
     private User user2;
@@ -39,16 +36,13 @@ public class UserDaoTest {
 
     @Before
     public void setUp() {
-        this.dao = this.context.getBean("userDao", UserDao.class);
-
-        this.user1 = new User("gyumee", "박성철", "springno1");
-        this.user2 = new User("leegw700", "이길원", "springno2");
-        this.user3 = new User("bumjin", "박범진", "springno3");
-
+        this.user1 = new User("gyumee", "�ڼ�ö", "springno1");
+        this.user2 = new User("leegw700", "�̱��", "springno2");
+        this.user3 = new User("bumjin", "�ڹ���", "springno3");
     }
 
     @Test
-    public void andAndGet() throws SQLException {
+    public void andAndGet() {
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
@@ -65,7 +59,7 @@ public class UserDaoTest {
         assertThat(userget2.getPassword(), is(user2.getPassword()));
     }
 
-    @Test(expected=EmptyResultDataAccessException.class)
+    @Test(expected = EmptyResultDataAccessException.class)
     public void getUserFailure() throws SQLException {
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
@@ -73,8 +67,9 @@ public class UserDaoTest {
         dao.get("unknown_id");
     }
 
+
     @Test
-    public void count() throws SQLException {
+    public void count() {
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
@@ -89,24 +84,24 @@ public class UserDaoTest {
     }
 
     @Test
-    public void getAll() throws SQLException {
+    public void getAll() {
         dao.deleteAll();
 
         List<User> users0 = dao.getAll();
         assertThat(users0.size(), is(0));
 
-        dao.add(user1);
+        dao.add(user1); // Id: gyumee
         List<User> users1 = dao.getAll();
         assertThat(users1.size(), is(1));
         checkSameUser(user1, users1.get(0));
 
-        dao.add(user2);
+        dao.add(user2); // Id: leegw700
         List<User> users2 = dao.getAll();
         assertThat(users2.size(), is(2));
         checkSameUser(user1, users2.get(0));
         checkSameUser(user2, users2.get(1));
 
-        dao.add(user3);
+        dao.add(user3); // Id: bumjin
         List<User> users3 = dao.getAll();
         assertThat(users3.size(), is(3));
         checkSameUser(user3, users3.get(0));
@@ -118,5 +113,28 @@ public class UserDaoTest {
         assertThat(user1.getId(), is(user2.getId()));
         assertThat(user1.getName(), is(user2.getName()));
         assertThat(user1.getPassword(), is(user2.getPassword()));
+    }
+
+    @Test(expected = DuplicateKeyException.class)
+    public void duplciateKey() {
+        dao.deleteAll();
+
+        dao.add(user1);
+        dao.add(user1);
+    }
+
+    @Test
+    public void sqlExceptionTranslate() {
+        dao.deleteAll();
+
+        try {
+            dao.add(user1);
+            dao.add(user1);
+        } catch (DuplicateKeyException ex) {
+            SQLException sqlEx = (SQLException) ex.getCause();
+            SQLExceptionTranslator set = new SQLErrorCodeSQLExceptionTranslator(this.dataSource);
+            DataAccessException transEx = set.translate(null, null, sqlEx);
+            assertThat(transEx, is(DuplicateKeyException.class));
+        }
     }
 }
